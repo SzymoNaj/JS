56067286d0b04e60fa617b0f85e8fcb879898b55
SzymonN <szymonnajdzion@gmail.com>
2024-11-21 09:02:04 +0100

    zadanie 1 js true/false

---
+++ zad1.js
@@ -0,0 +1,12 @@
+isOdd = function(x){
+    return x%2 != 0;
+}
+
+isEven = (x) => {
+    return x%2 == 0;
+}
+
+console.log(isOdd(6));
+console.log(isOdd(7));
+console.log(isEven(6));
+console.log(isEven(7));
